fb612eeb67c6271975471f8baf44e415ee813c8c
Bob Jacobsen <jacobsen@mac.com>
2021-12-25 14:47:44 -0500

    refactor

--- java/src/jmri/script/ScriptFileChooser.java
+++ java/src/jmri/script/ScriptFileChooser.java
@@ -12,8 +12,12 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import jmri.util.FileUtil;
 
 /**
+ * Create scripting-specific FileChooser.
+ *
+ * The primary content here is a set of scripting-specific filters.
  *
  * @author Randall Wood
+ * @author Bob Jacobsen    copyright 2021
  */
 public class ScriptFileChooser extends JFileChooser {
 
@@ -36,6 +40,27 @@ public class ScriptFileChooser extends JFileChooser {
         List<String> allExtensions = new ArrayList<>();
         HashMap<String, FileFilter> filters = new HashMap<>();
         List<String> filterNames = new ArrayList<>();
+
+        // add the ScriptEngine-managed files
+        addJmriScriptEngineManagerExtensions(allExtensions, filters, filterNames);
+
+        // add GraalVM specific
+        addGraalExtensions(allExtensions, filters, filterNames);
+
+        // create and install the actual filter
+        log.debug("allExtensions length is {}", allExtensions.size());
+        FileFilter allScripts = new FileNameExtensionFilter(Bundle.getMessage("allScripts"), allExtensions.toArray(new String[allExtensions.size()]));
+        this.addChoosableFileFilter(allScripts);
+        filterNames.stream().sorted().forEach((filter) -> {
+            this.addChoosableFileFilter(filters.get(filter));
+        });
+
+        // set initial configuration
+        this.setFileFilter(allScripts);
+        this.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    }
+
+    private void addJmriScriptEngineManagerExtensions(List<String> allExtensions, HashMap<String, FileFilter> filters, List<String> filterNames) {
         JmriScriptEngineManager.getDefault().getManager().getEngineFactories().stream().forEach((ScriptEngineFactory factory) -> {
             List<String> extensions = factory.getExtensions();
             allExtensions.addAll(extensions);
@@ -43,13 +68,14 @@ public class ScriptFileChooser extends JFileChooser {
             filterNames.add(name);
             filters.put(name, new FileNameExtensionFilter(name, extensions.toArray(new String[extensions.size()])));
         });
-        FileFilter allScripts = new FileNameExtensionFilter(Bundle.getMessage("allScripts"), allExtensions.toArray(new String[allExtensions.size()]));
-        this.addChoosableFileFilter(allScripts);
-        filterNames.stream().sorted().forEach((filter) -> {
-            this.addChoosableFileFilter(filters.get(filter));
-        });
-        this.setFileFilter(allScripts);
-        this.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    }
+
+    private void addGraalExtensions(List<String> allExtensions, HashMap<String, FileFilter> filters, List<String> filterNames) {
+        var name = "Python 3 Script Files";
+        var extension = "py3";
+        allExtensions.add(extension);
+        filters.put(name, new FileNameExtensionFilter(name, new String[]{extension}) );
+        filterNames.add(name);
     }
 
     private String fileForLanguage(String language) {
@@ -62,4 +88,5 @@ public class ScriptFileChooser extends JFileChooser {
             return language;
         }
     }
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ScriptFileChooser.class);
 }
