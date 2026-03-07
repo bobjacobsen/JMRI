c0ec3c4b7ec4c4d0790f9ac7fe919720f0da5b8c
Bob Jacobsen <jacobsen@mac.com>
2026-03-07 18:31:36 -0500

    setFontStyle at true end of editor load

--- java/src/jmri/jmrit/display/layoutEditor/configurexml/LayoutEditorXml.java
+++ java/src/jmri/jmrit/display/layoutEditor/configurexml/LayoutEditorXml.java
@@ -704,6 +704,14 @@ public class LayoutEditorXml extends AbstractXmlAdapter {
         panel.setVisible(true);    // always show the panel
         panel.resetDirty();
 
+        // make a pass over positionables, setting (re)setting their font style.
+        // This is to bypass a problem on Windows, where bold labels need to have
+        // this done after pack() to avoid sizing issues.
+        for (jmri.jmrit.display.Positionable label : panel.getContents()) {
+            var popup = label.getPopupUtility();
+            popup.setFontStyle(popup.getFontStyle());
+        }
+        
         // register the resulting panel for later configuration
         ConfigureManager cm = InstanceManager.getNullableDefault(jmri.ConfigureManager.class);
         if (cm != null) {
